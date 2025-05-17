3bbe7a5b7d744dfeea179c3da1b2d81fe4e70c99
Franklin Gonçalves <franklingoncalves@gmail.com>
2025-05-17 19:08:24 -0300

    test: add city field to `CreateOrg` use case tests

--- src/domain/use-cases/orgs/create-org.spec.ts
+++ src/domain/use-cases/orgs/create-org.spec.ts
@@ -15,6 +15,7 @@ describe('Create Org Use Case', () => {
   it('should be able to create a new org', async () => {
     const { org } = await sut.execute({
       name: 'Animal Rescue',
+      city: 'NYC',
       email: 'org@example.com',
       password: '123456',
       whatsapp: '123456789',
@@ -29,6 +30,7 @@ describe('Create Org Use Case', () => {
 
     await sut.execute({
       name: 'Org 1',
+      city: 'NYC',
       email,
       password: '123456',
       whatsapp: '123456789',
@@ -38,6 +40,7 @@ describe('Create Org Use Case', () => {
     await expect(() =>
       sut.execute({
         name: 'Org 2',
+        city: 'NYC',
         email,
         password: 'abcdef',
         whatsapp: '99999999',
